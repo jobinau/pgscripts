e8a40d0189b0743487a53eacbb9a12009cada461
jobinau <jobinau@gmail.com>
2020-11-23 13:39:28 +0530

    pgdg scripts

---
+++ pgdg_centos.sh
@@ -0,0 +1,55 @@
+###############################################################################
+##  Installation script for CentOS/Redhat on x86_64, Arm64
+##  Author : Jobin Augustine 
+##  Ref : https://www.postgresql.org/download/linux/redhat/
+###############################################################################
+
+export PGVER=$1
+if [ $USER != "postgres" ]; then
+    read -p "Installation as \"postgres\" user with sudo privillage is recommended. Would you like to continue installation as \"$USER\" user? " -n 1 -r
+    if [[ $REPLY =~ ^[Nn]$ ]]
+    then
+        echo "\nExiting the Installation"
+        exit 1
+    fi
+fi
+echo "Starting Installation"
+##use appropriate version number like 9.6 or 9.5
+#export PG=postgresql$PGVER
+##Remove "." from older versions
+export PG=postgresql`echo "$PGVER" | tr -d .`
+export OSVER=`cat /etc/*release | grep "VERSION_ID=" | cut -b 13`
+if [ $OSVER -ge "8" ]; then
+    CMD=dnf
+    sudo dnf -qy module disable postgresql
+else
+    CMD=yum
+fi
+ARCH=`uname -a | awk '{print $12}'`
+echo "https://download.postgresql.org/pub/repos/yum/$PGVER/redhat/rhel-$OSVER-$ARCH"
+sudo $CMD -y install https://download.postgresql.org/pub/repos/yum/reporpms/EL-$OSVER-$ARCH/pgdg-redhat-repo-latest.noarch.rpm
+sudo $CMD install -y $PG-server
+##sudo $CMD groupinstall -y postgresqldbserver$PGVER
+##Set Enviornment Variables
+export PATH=$PATH:/usr/pgsql-$PGVER/bin
+sed -i 's/PATH=.*/&:\/usr\/pgsql-'"$PGVER"'\/bin/' ~/.bash_profile
+## following can keep the PGBIN path to starting of the PATH
+## sed -r 's/(PATH=)(.+)/\1\/usr\/pgsql-'"$PGVER"'\/bin:\2/' ~/.bash_profile
+echo 'export PGVER='$PGVER >> ~/.bash_profile
+echo 'export PGBIN=/usr/pgsql-'"$PGVER"'/bin/' >> ~/.bash_profile
+echo 'export PGDATA=/var/lib/pgsql/'"$PGVER"'/data/' >> ~/.bash_profile
+
+#Contrib
+#sudo $CMD install -y $PG-contrib
+export PGDATA=/var/lib/pgsql/$PGVER/data/
+export PGSETUP_INITDB_OPTIONS="--data-checksums"
+sudo systemctl enable postgresql-$PGVER
+
+sudo /usr/pgsql-$PGVER/bin/postgresql*-setup initdb
+sed -i '/listen_addresses/c\listen_addresses = \x27*\x27' /var/lib/pgsql/$PGVER/data/postgresql.conf
+sed -i -e '/# IPv4 local connections:/a host    replication     replicator      192.168.50.0/24        md5\nhost    all     all      192.168.50.0/24        md5' $PGDATA/pg_hba.conf
+sudo systemctl start postgresql-$PGVER
+psql -c "ALTER USER postgres WITH PASSWORD 'vagrant'"
+
+psql -c "ALTER SYSTEM SET WAL_LEVEL=logical"
+sudo systemctl restart postgresql-$PGVER
---
+++ pgdg_ubuntu.sh
@@ -0,0 +1,27 @@
+
+##Environment Variables
+export PGVER=$1
+export PG=postgresql$PGVER
+export PATH=$PATH:/usr/lib/postgresql/$PGVER/bin
+##Edit the bashrc
+echo 'export PGBIN=/usr/lib/postgresql/'"$PGVER"'/bin/' >> ~/.bashrc
+#sed -i 's/PATH=.*/&:\/usr\/lib\/postgresql\/'"$PGVER"'\/bin/' ~/.bashrc
+echo 'export PATH=$PGBIN:$PATH' >> ~/.bashrc
+echo 'export PGDATA=/var/lib/postgresql/'"$PGVER"'/main/' >> ~/.bashrc
+
+##Installation
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo apt-get update
+sudo apt search postgresql-$PGVER
+sudo apt install -y postgresql-$PGVER
+
+##Parameter modifications
+sed -i '/listen_addresses/c\listen_addresses = \x27*\x27' /etc/postgresql/$PGVER/main/postgresql.conf
+sed -i '/max_wal_senders/c\max_wal_senders = 5' /etc/postgresql/$PGVER/main/postgresql.conf
+sed -i '/^#logging_collector/c\logging_collector = on' /etc/postgresql/$PGVER/main/postgresql.conf
+sed -i '/^#hot_standby/c\hot_standby = on' /etc/postgresql/$PGVER/main/postgresql.conf
+
+##Direct command
+/usr/lib/postgresql/$PGVER/bin/pg_ctl status -D /var/lib/postgresql/$PGVER/main -o '--config-file=/etc/postgresql/$PGVER/main/postgresql.conf'
+
