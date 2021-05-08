0463d76035f3744cde99715d2c79fdc476d34ef5
jobinau <jobinau@gmail.com>
2021-05-08 23:58:07 +0530

    Python3 compatibilty fixes, no longer supports python2

--- patroni/HAtester.py
+++ patroni/HAtester.py
@@ -1,25 +1,29 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 ############################### HAtester.py #####################################
-# 	Version 2.1    Jobin Augustine 2017
-# Program to test retry connecton on connection a connection failiure, 
-# Ctrl+C will result in  fresh connection to test load balancing.
+#     Version 2.1    Jobin Augustine 2017-2021
+# Program to test retry connection on a connection failure, 
+# Ctrl+C will result in terminating existing connection and fresh connection will be attempted
+#        This behavior is useful for testing load balancing.
+​
 
-#INSTALLATION AND RUN.
-# This needs  python-psycopg2 to be present in the system
+# This script needs  python-psycopg2 to be present in the system
 #Edit following connection string, with hostname, port and credentials
-connectionString = "host=localhost port=5432 dbname=postgres user=postgres password=vagrant connect_timeout=5"
-#./HAtester.py
+connectionString = "host=localhost port=5000 dbname=postgres user=postgres password=vagrant connect_timeout=5"
+
 #Execute Insert statement against table if doDML is true.
-#create a table in advance: CREATE TABLE HATEST (TM TIMESTAMP);
 doDML = True
 
+# create a table in advance for DML operation
+# CREATE TABLE HATEST (TM TIMESTAMP);
+
+​
 #STOP EXECUTION
 # Step 1 push the process to background : ctrl+z
 # Step 2 : pkill -9 HAtester.py
 #   OR  pgrep HAtester.py | xargs kill -9
-
+​
 ###############################################################################
-
+​
 import sys,time,psycopg2
 def create_conn():
    try:
@@ -29,7 +33,7 @@ def create_conn():
       print(e)
       return
    return conn
-
+​
 if __name__ == "__main__":
    conn = create_conn()
    if conn is not None:
@@ -45,31 +49,30 @@ if __name__ == "__main__":
             cur.execute("select pg_is_in_recovery(),inet_server_addr()")
             rows = cur.fetchone()
             if (rows[0] == False):
-               print "Working with Master " + rows[1],
+               print ("Working with Master " + rows[1]),
                if doDML:
                   cur.execute("INSERT INTO HATEST VALUES(CURRENT_TIMESTAMP) RETURNING TM")
                   if cur.rowcount == 1 :
                      conn.commit()
                      tmrow = str(cur.fetchone()[0])
-                     print 'Inserted ' + tmrow
+                     print ('Inserted ' + tmrow)
                else:
-                  print " No Attempt to insert data"
+                  print (" No Attempt to insert data")
             else:
-               print "Working with Slave " + rows[1],
+               print ("Working with Slave " + rows[1]),
                if doDML:
                   cur.execute("SELECT MAX(TM) FROM HATEST")
                   row = cur.fetchone()
-                  print " Retrived timestamp from database is " + str(row[0])
+                  print (" Retrived timestamp from database is " + str(row[0]))
                else:
-                  print " No Attempt to retrive data"
-
+                  print (" No Attempt to retrive data")
+​
          except:
-            print "Trying to connect"
+            print ("Trying to connect")
             if conn is not None:
                conn.close()
             conn = create_conn()
             if conn is not None:
-		         cur = conn.cursor()
-
+                 cur = conn.cursor()
+​
    conn.close()
-
