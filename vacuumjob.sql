1633b4fc7efc11c6f48ab07049ebb1928a7e7ab4
Jobin Augustine <jobinau@gmail.com>
2024-01-10 09:26:14 +0530

    added ANALYZE to the vaccum job and echo the command so that log can give more info

--- vacuumjob.sql
+++ vacuumjob.sql
@@ -1,5 +1,6 @@
+\set ECHO all
 WITH cur_vaccs AS (SELECT split_part(split_part(substring(query from '.*\..*'),'.',2),' ',1) as tab FROM pg_stat_activity WHERE query like 'autovacuum%')
-SELECT 'VACUUM FREEZE "'|| n.nspname ||'"."'|| c.relname ||'";'
+SELECT 'VACUUM (FREEZE,ANALYZE) "'|| n.nspname ||'"."'|| c.relname ||'";'
  FROM pg_class c
  JOIN pg_namespace n ON c.relnamespace = n.oid
  LEFT JOIN pg_class t ON c.reltoastrelid = t.oid and t.relkind = 't'
